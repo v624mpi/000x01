e17b97140e2b92ad7eefe1b035da86e242342c00
blahii <68820894+blahii@users.noreply.github.com>
2022-06-10 02:13:55 +0200

    0xp5x005_geometry

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 function setup() {
   createCanvas(400, 400);
+
 }
 
 function draw() {
   background(220);
+  //random size
+  A_001 = random (10,20,30,40);
+  A_002 = random (40,55,65,80);
+  random (A_001,A_002);
+  //geometry rect,circle,point
+  rect (100,100,random (A_001,A_002),random (A_001,A_002));
 }
