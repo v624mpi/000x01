5cf5b83721ce71573d9a8c917608bb64f61f8d68
blahii <68820894+blahii@users.noreply.github.com>
2022-06-10 05:13:30 +0200

    0xp5x0010_Glitch_C_A

--- sketch.js
+++ sketch.js
@@ -1,8 +1,7 @@
 let url = [
-	"https://coolors.co/ef476f-ffd166-06d6a0-118ab2-073b4c",
-	"https://coolors.co/ffa69e-faf3dd-b8f2e6-aed9e0-5e6472",
-	"https://coolors.co/f72585-7209b7-3a0ca3-4361ee-4cc9f0",
-	"https://coolors.co/d7263d-f46036-2e294e-1b998b-c5d86d",
+	"https://coolors.co/20222f-1a1f56-2c1e7d-118ab2-0134aa-0060e8-0297fb-00befa-83def3-d1ecf7-f2f6f9-fce503-fe6d02-e7011d-4b0f31",
+	// "https://coolors.co/0060e8-0297fb-00befa-83def3-d1ecf7",
+	// "https://coolors.co/f2f6f9-fce503-fe6d02-e7011d-4b0f31"
 ];
 let palette;
 let w = innerWidth;
@@ -13,48 +12,53 @@ var theta;
 
 function setup() {
   v = h
-	createCanvas(800, 800);
+	createCanvas(windowWidth, windowHeight);
+  	// createCanvas(500, 500);
 	// noSmooth();
-  smooth(5);
+  frameRate(60)
+  smooth(90);
   maxDiameter = 50; 
+  filter(POSTERIZE);
 	theta = 0; 
 }
 
 function draw() {
   var diam = 100 + sin(theta) * maxDiameter ;
+
 	randomSeed(frameCount / 500);
-	// blendMode(BLEND);
-	background(255);
-	copy(0,0,width,height,-1,-1,width+10,height+2);  
+	blendMode(BLEND);
+	// background('#20222f');
+	copy(0,0,width,height,-1,-1,width+2,height+ 2);  
 	palette = shuffle(createPalette(random(url)), true);
 	// background(0);
 	// blendMode(DIFFERENCE);
-	let offset = -width / 10;
-	let yStep = (height - offset *  v / random(10,50)) / random(2,20);
-	for (let y = offset; y <= height - offset; y += yStep) {
+	let offset = -windowWidth / 10;
+	let yStep = (windowHeight - offset *  diam / random(2,22)) / random(2,20);
+	for (let y = yStep; y <= height - offset; y += yStep) {
 
 		let num = int(1 + diam * noise(y, frameCount / 200));
 		let arr = [];
 		for (let i = 0; i < num; i++) {
-			let n = sq(sq(noise(y / 10, frameCount / 400))) * (width - offset * 2);
-			n = max(n, 1);
+			let n = sq(sq(noise(y / diam, frameCount / 400))) * (windowHeight - offset * 20);
+			n = max(n, 5);
 			arr.push(n);
 		}
 		drawingContext.setLineDash(arr);
-		drawingContext.lineDashOffset = y - frameCount / 200;
+		drawingContext.lineDashOffset = y - frameCount / 1000;
 		strokeWeight(yStep);
 		strokeCap(SQUARE);
-		// stroke(random(palette));
+    stroke(random(['#83def3','#d1ecf7','#f2f6f9','#fce503','#fe6d02','#e7011d','#4b0f31']));
+		// stroke(palette);
 		line(offset, y, width - offset, y);
 	}
+//width,yStep,diam
+	let xStep = (diam - offset * 1) / 7;
+	for (let x = yStep; x <= width - offset; x += xStep) {
 
-	let xStep = (width - offset * diam) / 7;
-	for (let x = offset; x <= width - offset; x += xStep) {
-
-		let num = int(1 + 20 * noise(x, frameCount / 200));
+		let num = int(1 + diam * noise(x, frameCount / 100));
 		let arr = [];
 		for (let i = 0; i < num; i++) {
-			let n = sq(sq(noise(x / 10, frameCount /  v / random(10,299)))) * (width - offset * 2);
+			let n = sq(sq(noise(x / diam, frameCount / 900))) * (width - offset * 2);
 			n = max(n, 1);
 			arr.push(n);
 		}
@@ -62,25 +66,9 @@ function draw() {
 		drawingContext.lineDashOffset = x - frameCount / 200;
 		strokeWeight(xStep);
 		strokeCap(SQUARE)
+    stroke(random(['#20222f','#1a1f56','#2c1e7d','#118ab2','#0134aa','#0060e8','#0297fb','#00befa']));
 		// stroke(random(palette));
 		line(x, offset, x, height - offset * 2);
-	}
-
-
-	for (let y = offset; y <= height / 2 - offset; y += yStep) {
-
-		let num = int(1 + 10 * noise(y, frameCount / 100));
-		let arr = [];
-		for (let i = 0; i < num; i++) {
-			let n = sq(sq(noise(y / 10, frameCount / 200))) * (width - offset * 2);
-			n = max(n, 1);
-			arr.push(n);
-		}
-		drawingContext.setLineDash(arr);
-		drawingContext.lineDashOffset = y - frameCount / 100;
-		strokeWeight(yStep);
-		strokeCap(SQUARE)
-		line(offset, y , width - offset, y);
 	}
 
 }
@@ -95,4 +83,8 @@ function createPalette(_url) {
 		arr[i] = color('#' + arr[i]);
 	}
 	return arr;
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
