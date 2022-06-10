e52932ae4beb7161fc040a553480517f58e45b78
blahii <68820894+blahii@users.noreply.github.com>
2022-06-10 16:58:29 +0200

    glitch

--- sketch.js
+++ sketch.js
@@ -9,10 +9,18 @@ let h = innerHeight;
 let  num = 20;
 var maxDiameter; 
 var theta; 
+const points=[]
+const n=5
+let   t=0
 
 function setup() {
   v = h
+  
 	createCanvas(windowWidth, windowHeight);
+
+  for(i=0;i<w*n;i++){
+		points[i]=createVector(random(width),random(height))  // Create all the points
+	}
   	// createCanvas(500, 500);
 	// noSmooth();
   frameRate(60)
@@ -26,20 +34,20 @@ function draw() {
   var diam = 100 + sin(theta) * maxDiameter ;
 
 	randomSeed(frameCount / 500);
-	blendMode(BLEND);
+	blendMode(OVERLAY);
 	// background('#20222f');
 	copy(0,0,width,height,-1,-1,width+2,height+ 2);  
 	palette = shuffle(createPalette(random(url)), true);
 	// background(0);
-	// blendMode(DIFFERENCE);
-	let offset = -windowWidth / 10;
-	let yStep = (windowHeight - offset *  diam / random(2,22)) / random(2,20);
+	blendMode(DIFFERENCE);
+	let offset = -windowWidth / 2;
+	let yStep = (windowHeight - offset *  diam / random(10,22)) / random(2,20);
 	for (let y = yStep; y <= height - offset; y += yStep) {
 
-		let num = int(1 + diam * noise(y, frameCount / 200));
+		let num = int(1 + diam * noise(y, frameCount / 500));
 		let arr = [];
 		for (let i = 0; i < num; i++) {
-			let n = sq(sq(noise(y / diam, frameCount / 400))) * (windowHeight - offset * 20);
+			let n = sq(sq(noise(y / diam, frameCount / 400))) * (windowHeight - offset * 30);
 			n = max(n, 5);
 			arr.push(n);
 		}
@@ -58,7 +66,7 @@ function draw() {
 		let num = int(1 + diam * noise(x, frameCount / 100));
 		let arr = [];
 		for (let i = 0; i < num; i++) {
-			let n = sq(sq(noise(x / diam, frameCount / 900))) * (width - offset * 2);
+			let n = sq(sq(noise(x / diam, frameCount / 900))) * (width - offset * 10);
 			n = max(n, 1);
 			arr.push(n);
 		}
@@ -71,7 +79,36 @@ function draw() {
 		line(x, offset, x, height - offset * 2);
 	}
 
+  for(let p of points){ // For all the points
+    
+    // Flow Field -> Uses 2D noise value (0-1) as a direction for the point
+    // In this case we use trigonometric values (TWO_PI) but you can use Degrees too.
+    // And coding chads can use noiseScale instead of my *0.01
+    let n=noise(p.x*0.01,p.y*0.01)*TWO_PI
+    
+    
+    // Magic      aka "Constrains The number of directions the noise value can have"
+	  m=5                // M for Marvellously-Majestic-Magnificient-Modifying-Magic
+	  a=TWO_PI/m          // Change direction
+    b=round(n/TWO_PI*m) // Round the value
+    n=a*b;              // New direction
+    
+    // Moving the points -> because we use trigonometry we can use cos and sin to move
+    // To make it move faster you can multiple the values
+    // Swaping cos/sin and reversing the sign (+ to - or - to +) rotates where they move
+	  p.add(cos(n)*random(2,40),sin(n))
+    
+    // Colours !
+	  if (n-5>1) stroke(255,0,0,t);
+    else stroke(0,0,255,t)
+    
+    // Plot the point
+	  circle(p.x,p.y) // Use circles please. Don't be like me
+  }
+  
+  t++; // Time ticking :)
 }
+
 
 
 
@@ -88,3 +125,8 @@ function createPalette(_url) {
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
+
+
+
+
+
